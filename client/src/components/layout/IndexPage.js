1b2e2c5fd71ed4755952426bf310707e61be34a4
pcadle <paul.cadle1@gmail.com>
2022-02-17 11:47:20 -0500

    search bar on homepage to load up default location

--- client/src/components/layout/IndexPage.js
+++ client/src/components/layout/IndexPage.js
@@ -3,6 +3,7 @@ import MapContainer from './MapContainer'
 import RouteTile from './RouteTile'
 
 const IndexPage = (props) => {
+debugger
   const [routes, setRoutes] = useState([])
   const [location, setLocation] = useState({
     zipcode: '',
@@ -25,9 +26,13 @@ const IndexPage = (props) => {
       console.log(`Error in fetch: ${error}`)
     }
   }
-
+  
   useEffect(() => {
-    getAllRoutes()
+    if(props.location.state){
+      getAllRoutes(props.location.state.searchLocation)
+    }else{
+      getAllRoutes(location)
+    }
   }, [])
 
   const handleSubmit = (event) => {
--- client/src/components/layout/LandingPage.js
+++ client/src/components/layout/LandingPage.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router'
 const LandingPage = (props) => {
+  const [searchLocation, setSearchLocation] = useState({
+    zipcode: '',
+    radius: 25
+  })
+  const [shouldRedirect, setShouldRedirect] = useState(false)
+
+  const handleChange = (event) => {
+    setSearchLocation({...searchLocation, zipcode: event.currentTarget.value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setShouldRedirect(true)
+  }
+
+  if(shouldRedirect){
+    return <Redirect to={{
+      pathname: "/routes",
+      state: { searchLocation }
+    }} />
+  }
   return (
     <div className="landing-page">
       <h1 className='landing-header'>Find Routes, Partners, and Share Your Adventures</h1>
@@ -9,6 +31,12 @@ const LandingPage = (props) => {
           Sign Up
         </Link>
       </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="zip">Find Climbs Near You!</label>
+        <input type="text" id="zip" name="zip" placeholder="ZipCode" onChange={handleChange} value={searchLocation.zipcode}></input>
+        <input type="submit"></input>
+      </form>
+
       <div className="tiles">
         <div className="tile">
           <Link to="/routes">Find Climbing Destinations!</Link>
