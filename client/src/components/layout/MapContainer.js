b7a45f365c47320c97fa9aa34fdeaf0ee1ac21f6
pcadle <paul.cadle1@gmail.com>
2022-02-25 16:03:28 -0500

    removed import

--- client/src/components/layout/MapContainer.js
+++ client/src/components/layout/MapContainer.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api'
-import REACT_APP_MAP_KEY from '../../../local_key'
 const MapContainer = (props) => {
 
   const mapDimensions = {
